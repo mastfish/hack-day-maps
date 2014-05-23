6800d3cfcdd021331a46e3edab306237b7394d3a
Justin Lambert <jlambert@au.westfield.com>
2014-05-23 10:28:46 +1000

    beaconing

---
+++ app/src/main/java/com/westfield/myapplication2/app/MonitoringActivity.java
@@ -0,0 +1,55 @@
+package com.westfield.myapplication2.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.radiusnetworks.ibeacon.IBeaconConsumer;
+import com.radiusnetworks.ibeacon.IBeaconManager;
+import com.radiusnetworks.ibeacon.MonitorNotifier;
+import com.radiusnetworks.ibeacon.Region;
+
+/**
+ * Created by mastfish on 23/05/2014.
+ */
+public class MonitoringActivity extends Activity implements IBeaconConsumer {
+    protected static final String TAG = "RangingActivity";
+    private IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranging);
+        iBeaconManager.bind(this);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        iBeaconManager.unBind(this);
+    }
+    @Override
+    public void onIBeaconServiceConnect() {
+        iBeaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Log.i(TAG, "I just saw an iBeacon for the firt time!");
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i(TAG, "I no longer see an iBeacon");
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+                Log.i(TAG, "I have just switched from seeing/not seeing iBeacons: "+state);
+            }
+        });
+
+        try {
+            iBeaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+        } catch (RemoteException e) {   }
+    }
+
+}
